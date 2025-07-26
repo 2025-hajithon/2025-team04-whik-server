e13e553e72c186addb06381d6dda2d5b92f43104
phdcoco <84033556+phdcoco@users.noreply.github.com>
2025-07-26 22:33:11 +0900

    feat: DestinaionCategory 엔티티 구현 (#15)

---
+++ src/main/java/com/example/whik/DestinationCategory.java
@@ -0,0 +1,38 @@
+package com.example.whik;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DestinationCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "destination_category_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "destination_id")
+    private Destination destination;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private DestinationCategory(Destination destination, Category category) {
+        this.destination = destination;
+        this.category = category;
+    }
+
+    public static DestinationCategory create(Destination destination, Category category) {
+        return DestinationCategory.builder()
+                .destination(destination)
+                .category(category).build();
+    }
+
+
+}
