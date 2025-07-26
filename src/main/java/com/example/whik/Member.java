403969997a079d25702247676c0f90aacd2c481e
phdcoco <claireluna470@gmail.com>
2025-07-26 21:31:47 +0900

    feat: UUID 생성 전략 변경, ddl 추가

--- src/main/java/com/example/whik/Member.java
+++ src/main/java/com/example/whik/Member.java
@@ -2,17 +2,14 @@ package com.example.whik;
 
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-import org.hibernate.validator.constraints.UUID;
+import java.util.UUID;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id
-    @GeneratedValue // 전략을 쓰지 않고 다음 줄에 있는 어노테이션 사용
-    @JdbcTypeCode(SqlTypes.UUID) // UUID 생성 관련 어노테이션
-    private UUID id;
+    private final UUID id = UUID.randomUUID();
 
     @Column(unique = true) // unique 조건 추가
     private String nickname;
