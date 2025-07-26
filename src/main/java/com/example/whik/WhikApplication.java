9e3a06189166f197347fd44436a62410cfaf04c8
phdcoco <claireluna470@gmail.com>
2025-07-27 02:09:14 +0900

    fix: WhikApplication 위치 변경

--- src/main/java/com/example/whik/WhikApplication.java
+++ src/main/java/com/example/whik/WhikApplication.java
@@ -1,4 +1,4 @@
-package com.example.whik.entity;
+package com.example.whik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
