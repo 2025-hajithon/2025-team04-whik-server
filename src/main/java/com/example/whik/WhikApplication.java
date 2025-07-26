e3a25eb51f5aef28f96e455d851acb5759203857
chosw1002@naver.com <chosw1002@naver.com>
2025-07-26 18:01:52 +0900

    first commit

---
+++ src/main/java/com/example/whik/WhikApplication.java
@@ -0,0 +1,13 @@
+package com.example.whik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhikApplication.class, args);
+	}
+
+}
