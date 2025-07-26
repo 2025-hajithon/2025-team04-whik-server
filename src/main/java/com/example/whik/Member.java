ee98ceceec45824bda2358ecc57dfa5ffcce457b
phdcoco <84033556+phdcoco@users.noreply.github.com>
2025-07-26 21:33:51 +0900

    feat: Member 엔티티 구현 (#6)
    * feat: Member 엔티티 구현
    
    * feat: UUID 생성 전략 변경, ddl 추가

---
+++ src/main/java/com/example/whik/Member.java
@@ -0,0 +1,26 @@
+package com.example.whik;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+    @Id
+    private final UUID id = UUID.randomUUID();
+
+    @Column(unique = true) // unique 조건 추가
+    private String nickname;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Member(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public static Member create(String nickname) {
+        return Member.builder()
+                .nickname(nickname).build();
+    }
+}
